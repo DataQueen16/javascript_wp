3900855038a01b74637f9697870e39ebca9c8779
Dataqueen16 <dataqueen16@gmail.com>
2019-11-26 17:25:32 -0600

    Playing with value based on execution stack & scope

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 
-var a = 'Hello World!';
-
 function b() {
-    console.log('Called b!');
+    var myVar;
+    console.log('myVar inside b: ', myVar);
 }
 
+function a() {
+    var myVar = 2;
+    console.log('myVar inside a: ', myVar);
+    b();
+}
+
+var myVar = 1;
+console.log('myVar initial value: ', myVar);
+a();
+
+
 // Don't use hoisting/rely on it to access variables and functions
-b();
-console.log(a);
+// console.log(a);
+
+// var a = 'Hello World!';
+// console.log(a);
+
+// if (a === undefined) {
+//     console.log('a is undefined');
+// } else {
+//     console.log('a is defined');
+// }
